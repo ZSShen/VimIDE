0eb94e3ffecc207438386d3c0135851c0fe7d865
ZSShen <andy780328@hotmail.com>
2017-01-21 06:16:29 -0800

    Add: Install the YCM Clang completer in automation script.

--- install.sh
+++ install.sh
@@ -9,13 +9,24 @@
 
 # Install Vundle plugin.
 echo "Installing Vundle ..."
+echo ""
 {
     git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-} > /dev/null
+} &> /dev/null
 
 # Install VimIDE included 3 party plugins.
 echo "Installing VimIDE included plugins ..."
+echo "Within 5 minutes ..."
+echo ""
 {
     vim +PluginInstall +qall
+} &> /dev/null
+
+echo "Installing YCM Clang completer ..."
+echo "Need 15 to 20 minutes ..."
+echo ""
+{
+    cd ~/.vim/bundle/YouCompleteMe
+    ./install.py --clang-completer
 } > /dev/null
 
