b75cdde30075000f7200abc33feb8da24b1073a5
ZSShen <andy780328@hotmail.com>
2017-01-21 05:28:56 -0800

    Add: Add the draft demo screenshot.

--- install.sh
+++ install.sh
@@ -2,15 +2,20 @@
 
 
 # Copy the VimIDE configuration.
-cp .vimrc ~/.vimrc
-cp .ycm_extra_conf.py ~/.ycm_extra_conf
-
+{
+    cp .vimrc ~/.vimrc
+    cp .ycm_extra_conf.py ~/.ycm_extra_conf
+} &> /dev/null
 
 # Install Vundle plugin.
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
+echo "Installing Vundle ..."
+{
+    git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+} > /dev/null
 
 # Install VimIDE included 3 party plugins.
-vim +PluginInstall +qall
-
+echo "Installing VimIDE included plugins ..."
+{
+    vim +PluginInstall +qall
+} > /dev/null
 
